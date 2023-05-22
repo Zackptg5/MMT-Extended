5648ce24a2011a83ab4e3fabd8c4e3eaac033d9e
konradmb <konradmb@o2.pl>
2023-05-22 03:07:29 +0200

    Update functions.sh

--- common/functions.sh
+++ common/functions.sh
@@ -121,7 +121,7 @@ ui_print " "
 [ -z $DYNLIB ] && DYNLIB=false
 [ -z $DEBUG ] && DEBUG=false
 INFO=$NVBASE/modules/.$MODID-files
-ORIGDIR="$MAGISKTMP/mirror"
+ORIGDIR="$MAGISKTMP/.magisk/mirror"
 if $DYNLIB; then
   LIBPATCH="\/vendor"
   LIBDIR=/system/vendor
